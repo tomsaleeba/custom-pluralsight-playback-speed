2aaa7609df2e186147b5b11e36f2d19881db5be8
Tom Saleeba <tom.saleeba@gmail.com>
2019-11-25 13:48:57 +1030

    fix: try to update to support the newest player but on-the-fly speed changing isn't currently working, a page refresh is required

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # custom-pluralsight-playback-speed
-Tampermonkey script to allow custom playback speeds in PluralSight site
+Tampermonkey script to allow custom playback speeds in PluralSight site.
+
+**note:** right now, we can't change the speed on-the-fly like we used to.
+Until that gets figured out, just hit the speed you want, then refresh the page
+and it'll take effect.
 
 ## Why
-Some videos are too slow so it's nice to be able to get playback speed > 2x plus it's helpful to have the speed control up front rather than in a menu.
+Some videos are too slow so it's nice to be able to get playback speed > 2x
+plus it's helpful to have the speed control up front rather than in a menu.
 
 ## Read more
-https://techotom.wordpress.com/2016/12/06/pluralsight-custom-playback-speeds/
+https://blog.techotom.com/software%20development/2016/12/06/pluralsight-custom-playback-speeds.html
--- tampermonkey-script.js
+++ tampermonkey-script.js
@@ -1,71 +1,106 @@
 // ==UserScript==
 // @name PluralSight custom speeds
 // @namespace https://techotom.wordpress.com
-// @version 0.2
+// @version 0.3
 // @description more fasterer
 // @author Tom
-// @match https://app.pluralsight.com/player*
+// @match https://app.pluralsight.com/course-player*
 // @grant none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+;(function() {
+  'use strict'
 
-    var console = console || {};
-    var className = 'speed-selector';
-    function resetBoldness() {
-        var speedSelectors = document.getElementsByClassName(className);
-        for (var i = 0;i < speedSelectors.length;i++) {
-            var curr = speedSelectors[i];
-            curr.style.fontWeight = 'normal';
-        }
+  var console = console || {}
+  var className = 'speed-selector'
+  function resetBoldness() {
+    var speedSelectors = document.getElementsByClassName(className)
+    for (var i = 0; i < speedSelectors.length; i++) {
+      var curr = speedSelectors[i]
+      curr.style.fontWeight = 'normal'
     }
+  }
 
-    function appendSpeedControl(div, speed) {
-        var speedAnchor = document.createElement("a");
-        speedAnchor.style.display = "block";
-        speedAnchor.style.cursor = "pointer";
-        speedAnchor.onclick = function() {
-            localStorage.setItem("playbackRate", (speed));
-            document.getElementById('play-control').click();
-            document.getElementById('play-control').click();
-            resetBoldness();
-            this.style.fontWeight = "bold";
-        };
-        speedAnchor.classList.add('speed-selector');
-        var label = document.createTextNode(speed + "x");
-        speedAnchor.appendChild(label);
-        div.appendChild(speedAnchor);
+  function appendSpeedControl(div, speed) {
+    var speedAnchor = document.createElement('a')
+    speedAnchor.style.display = 'block'
+    speedAnchor.style.cursor = 'pointer'
+    speedAnchor.onclick = function() {
+      // FIXME this doesn't work to change the speed on-the-fly as of
+      // 2019-Nov-25. There's an `onSeek` function (line 26131 of the
+      // formatted, minified 0.0.70 embeddable-player code) that is capable of
+      // changing the speed if you use the debugger to change the value of
+      // playbackSpeed. Getting inside the closure seems to be a challenge
+      // though. Otherwise, we need to find a way to make the player read the
+      // fresh values from localStorage. It does change the speed if you
+      // refresh the page though. Not ideal but it works.
+      var playerSettings = JSON.parse(
+        localStorage.getItem('ps-embeddable-player-settings'),
+      )
+      playerSettings.playbackSpeed = speed
+      localStorage.setItem(
+        'ps-embeddable-player-settings',
+        JSON.stringify(playerSettings),
+      )
+      getPlayPauseButton().click()
+      getPlayPauseButton().click()
+      resetBoldness()
+      this.style.fontWeight = 'bold'
     }
+    speedAnchor.classList.add('speed-selector')
+    var label = document.createTextNode(speed + 'x')
+    speedAnchor.appendChild(label)
+    div.appendChild(speedAnchor)
+  }
 
-    var css = '.techotom-speed-control { opacity: 0.1; } .techotom-speed-control:hover { opacity: 0.8; }';
-    var head = document.head || document.getElementsByTagName('head')[0];
-    var style = document.createElement('style');
-    style.type = 'text/css';
-    style.appendChild(document.createTextNode(css));
-    head.appendChild(style);
+  function getPlayPauseButton() {
+    var playBtn = document.querySelectorAll('[data-text="Play (k)"] button')[0]
+    if (playBtn) {
+      return playBtn
+    }
+    var pauseBtn = document.querySelectorAll(
+      '[data-text="Pause (k)"] button',
+    )[0]
+    if (pauseBtn) {
+      return pauseBtn
+    }
+    throw new Error(
+      'Cannot find the play/pause button. The UI has ' +
+        'probably changed and we need a code update to follow it.',
+    )
+  }
 
-    var body = document.getElementsByTagName('body')[0];
-    var div = document.createElement("div");
-    div.classList = 'techotom-speed-control';
-    div.style.position = "fixed";
-    div.style.margin = "1em";
-    div.style.fontSize = "1.5em";
-    div.style.background = "#FFF";
-    div.style.zIndex = "2";
-    div.style.width = "3em";
-    div.style.marginTop = "6em";
-    appendSpeedControl(div, 1);
-    appendSpeedControl(div, 1.2);
-    appendSpeedControl(div, 1.3);
-    appendSpeedControl(div, 1.5);
-    appendSpeedControl(div, 1.7);
-    appendSpeedControl(div, 1.8);
-    appendSpeedControl(div, 2);
-    appendSpeedControl(div, 2.1);
-    appendSpeedControl(div, 2.2);
-    appendSpeedControl(div, 2.5);
-    appendSpeedControl(div, 2.7);
-    appendSpeedControl(div, 3);
-    body.insertBefore(div, body.childNodes[0]);
-})();
+  var css =
+    '.techotom-speed-control { opacity: 0.1; } .techotom-speed-control:hover { opacity: 0.8; }'
+  var head = document.head || document.getElementsByTagName('head')[0]
+  var style = document.createElement('style')
+  style.type = 'text/css'
+  style.appendChild(document.createTextNode(css))
+  head.appendChild(style)
+
+  var body = document.getElementsByTagName('body')[0]
+  var div = document.createElement('div')
+  div.classList = 'techotom-speed-control'
+  div.style.position = 'fixed'
+  div.style.margin = '1em'
+  div.style.fontSize = '1.5em'
+  div.style.background = '#FFF'
+  div.style.zIndex = '99999'
+  div.style.width = '3em'
+  div.style.marginTop = '6em'
+  appendSpeedControl(div, 1)
+  appendSpeedControl(div, 1.2)
+  appendSpeedControl(div, 1.3)
+  appendSpeedControl(div, 1.5)
+  appendSpeedControl(div, 1.7)
+  appendSpeedControl(div, 1.8)
+  appendSpeedControl(div, 2)
+  appendSpeedControl(div, 2.1)
+  appendSpeedControl(div, 2.2)
+  appendSpeedControl(div, 2.5)
+  appendSpeedControl(div, 2.7)
+  appendSpeedControl(div, 3)
+  appendSpeedControl(div, 3.14)
+  appendSpeedControl(div, 3.5)
+  body.insertBefore(div, body.childNodes[0])
+})()
